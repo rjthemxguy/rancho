b12393a26d2070eb190bc74d01154e4549cca984
RJ Robinson <rj@localhost.localdomain>
2023-03-12 12:56:45 -0700

    Chart Div fixes

--- src/App.js
+++ src/App.js
@@ -4,13 +4,49 @@ import  {Map} from "./Map/Map"
 import Legend from "./Legend"
 import BarChart from './BarChart';
 import "leaflet/dist/leaflet.css"
+import { Bar } from "react-chartjs-2"
+  
+
+export const options = {
+  responsive: true,
+  maintainAspectRatio: false,
+  plugins: {
+    legend: {
+    position: 'bottom',
+   
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Bar Chart',
+    },
+  },
+};
+
+const labels = ["Clark", "Stickler", "Olmsted", "Hannah", "Henderson", "Jimenez"]
+
+
+
+export const data = {
+labels,
+datasets: [
+  {
+    label: 'Votes',
+    data: ["50", "10","70", "50", "30", "29"],
+    backgroundColor: ["red", "blue", "green"]
+  }
+ 
+],
+};
+
 
 function App() {
   return (
     <>
    <Map/>
    <Legend/>
-   <BarChart/>
+   
+   <BarChart data={data} options={options}/>
+        
    </> 
   );
 }
--- src/BarChart.js
+++ src/BarChart.js
@@ -1,9 +1,35 @@
 import "./barChart.css"
+import {
+    Chart as ChartJS,
+    BarElement,
+    CategoryScale,
+    LinearScale,
+    Legend
+  } from "chart.js"
 
-const BarChart = () => {
+  import { Bar } from "react-chartjs-2"
+  
+
+
+
+  ChartJS.register(
+    BarElement,
+    CategoryScale,
+    LinearScale,
+    Legend
+  )
+
+  
+
+const BarChart = ({data, options}) => {
     return (
         <div className="barChart">
-            <h1>Test</h1>
+            <Bar
+        data = {data}
+        options = {options}>
+       
+
+      </Bar>
         </div>
     );
 }
